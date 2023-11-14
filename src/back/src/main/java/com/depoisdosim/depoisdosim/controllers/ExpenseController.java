60018dd91db2807a406f9daaf42a080239cd7ea2
Stephany Sara De Oliveira Santos <1192055@sga.pucminas.br>
2023-11-14 14:14:13 -0300

    Update: create expense.java

---
+++ src/back/src/main/java/com/depoisdosim/depoisdosim/controllers/ExpenseController.java
@@ -0,0 +1,61 @@
+package com.depoisdosim.depoisdosim.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.depoisdosim.depoisdosim.models.Expense;
+import com.depoisdosim.depoisdosim.services.ExpenseService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/expense")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> findById(@PathVariable Long id) {
+        Expense obj = this.expenseService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Expense>> findAllByWeddingId(@PathVariable Long weddingId) {
+        List<Expense> objs = this.expenseService.findAllByWeddingId(weddingId);
+        return ResponseEntity.ok().body(objs);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody Expense obj) {
+        this.expenseService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody Expense obj, @PathVariable Long id) {
+        obj.setId(id);
+        this.expenseService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.expenseService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
